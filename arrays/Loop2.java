f52f5fabcae55485d1ee8c52a3df0ac34d9ec3b9
Manjunath Koralli <mskoralli@gmail.com>
2020-01-28 22:17:18 +0530

    Loop

--- arrays/Loop2.java
+++ arrays/Loop2.java
@@ -5,7 +5,8 @@ public class Loop2 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		 int number=20;  
+		 Loop2 loop2 = new Loop2();
+		 int number=50;  
 		     
 		 switch(number){  
 		     
@@ -18,6 +19,18 @@ public class Loop2 {
 		      
 		    default:System.out.println("Not in 10, 20 or 30");  
 		 } 
+		 
+		 /*switch(loop2){  
+	     
+		    case 10: System.out.println("10");  
+		    break;  
+		    case 20: System.out.println("20");  
+		    break;  
+		    case 30: System.out.println("30");  
+		    break;  
+		      
+		    default:System.out.println("Not in 10, 20 or 30");  
+		 } */
 		 
 		 for(int i=1;i<=10;i++){  
 			 if(i==5){  
